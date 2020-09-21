baf24aa5228ee1ee16f9292414a36eac13fe60a8
Andres Tahmizian <andrestahmizian@gmail.com>
2020-09-21 12:52:23 -0300

    Actualziado

---
+++ js/zombie_conductor.js
@@ -0,0 +1,34 @@
+/* Para insipirarte para la implementacion del ZombieConductor podes usar
+al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
+Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
+no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
+del ZombieCaminante que eran los mismos. */
+
+var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov,esHorizontal/*, parametro/s extra de ZombieConductor*/) {
+  /* Completar constructor a partir de Enemigo */
+  this.esHorizontal = esHorizontal;
+  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);
+  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+  this.potencia = 5;
+}
+ZombieConductor.prototype = Object.create(Enemigo.prototype);
+ZombieConductor.prototype.constructor = ZombieConductor;
+/* Completar creacion del ZombieConductor */
+ZombieConductor.prototype.mover = function(){
+  if(this.esHorizontal){
+    this.x -= this.velocidad;
+  }else{
+    this.y += this.velocidad;
+  }
+ 
+  if ((this.x < this.rangoMov.desdeX) || (this.x > this.rangoMov.hastaX)){
+    this.velocidad *= -1;
+  }
+  if ((this.y < this.rangoMov.desdeY) || (this.y > this.rangoMov.hastaY)) {
+    this.velocidad *= -1;
+  }
+}
+/* Completar metodos para el movimiento y el ataque */
+ZombieConductor.prototype.atacar = function(jugador) {
+  jugador.perderVidas(this.potencia);
+ }
